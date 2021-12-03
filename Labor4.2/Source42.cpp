57876b9a22b0c3ba57a9c0611c18bb1204c6f759
Oleksandr Oleniuk <oleksandr.oleniuk.it.2021@lpnu.ua>
2021-12-03 11:54:30 +0200

    First commit master

---
+++ Labor4.2/Source42.cpp
@@ -0,0 +1,41 @@
+// Source42.cpp
+// < Оленюк Олександр >
+// Лабораторна робота 4.2
+// Табуляція функції, заданої формулою: функція однієї змінної.
+// Варіант 8
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 2 + 6 * x;
+		if (x <= 0)
+			B = log(cos(x)) + pow(x, 5);
+		else
+			if (x > 3)
+				B = 12 * x - pow(x, 8);
+			else
+				B = 1 / tan((1 + log(x)) / 3);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
